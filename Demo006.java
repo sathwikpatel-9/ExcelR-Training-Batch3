9c8af5f5f173b24f5387de8015a6ca71542c95f9
sathwikpatel-9 <sathwikpatel6777@gmail.com>
2025-01-07 12:07:54 +0530

    Add files via upload

---
+++ Demo006.java
@@ -0,0 +1,11 @@
+package Core_Java;
+public class Demo006{
+    public static void main(String[] args){
+        String name = "Anirudh";
+        String pwd = "Anirudh@15";
+        while((name == "Anirudh") && (pwd == "Anirudh@15")){
+            System.out.println("Hello Anirudh welcome Malla Reddy University");
+            break;
+        }
+    }
+}
