998e56053462f738d20c7c61359d6d5a2e7ae097
sathwikpatel-9 <sathwikpatel6777@gmail.com>
2025-01-25 12:48:59 +0530

    To print 5th table.

---
+++ Demo050.java
@@ -0,0 +1,10 @@
+class TableOfFive {
+    public static void main(String[] args) {
+        int number = 5;
+
+        System.out.println("Multiplication Table of " + number + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
