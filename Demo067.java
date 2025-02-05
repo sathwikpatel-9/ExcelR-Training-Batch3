b31d0d8d64f5475e7070ed4d7cdf93c761c06087
sathwikpatel-9 <sathwikpatel6777@gmail.com>
2025-02-05 12:53:34 +0530

    To print even elements of an Array

---
+++ Demo067.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class PrintEvenElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.println("Even elements in the array are:");
+        for (int i = 0; i < size; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.print(array[i] + " ");
+            }
+        }
+
+        scanner.close();
+    }
+}
